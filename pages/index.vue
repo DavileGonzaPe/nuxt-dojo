2117925327f63f68a7dfd51424ed52890b9ad183
David González <dp@phoenixconnection.es>
2023-05-09 18:56:17 +0200

    Adding pages

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h2>Home</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente non nesciunt maiores, id perspiciatis corporis
+            molestias officia consectetur illum consequuntur.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente non nesciunt maiores, id perspiciatis corporis
+            molestias officia consectetur illum consequuntur.</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    h2 {
+        margin-bottom: 20px;
+        font-size: 36px;
+    }
+
+    p {
+        margin: 20px 0;
+    }
+</style>
