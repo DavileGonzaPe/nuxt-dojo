6e9d176d5ed26b0410ad865882f0fde68ee59cf4
David González <dp@phoenixconnection.es>
2023-05-09 19:02:12 +0200

    Route parameters

---
+++ pages/products/[id].vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <p>Product details for {{ id }}</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rem incidunt iste eius, nostrum esse dolorum aut
+            maiores repellat quod dolore!</p>
+    </div>
+</template>
+
+<script setup>
+    const { id } = useRoute().params
+</script>
+
+<style scoped>
+
+</style>
